6c3b361ed2c7186e418640b7d635beb5ca732909
SeanBrackenborough <sean.brackenborough.dev@gmail.com>
2022-12-09 22:14:32 +0000

    - Updated for to include my details - Added "plyer" install requirement

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+import sys
 from os import path
+
+from setuptools import find_packages, setup
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-description = "Tomato Clock is a simple command line pomodoro app"
-version = "0.0.10"
+DESCRIPTION = "Tomato Clock is a simple command line pomodoro app"
+VERSION = "1.0.0"
 
 setup(
-    name="tomato-clock",
-    version=version,
-    author="Bruce Lee",
-    author_email="bruce.meerkat@gmail.com",
-    description=description,
+    name="tomato-clock-extended",
+    version=VERSION,
+    author="Sean Brackenborough",
+    author_email="sean.brackenborough.dev@gmail.com",
+    description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
     license="MIT",
-    keywords="pomodoro tomato tomato-timer terminal terminal-app pomodoro-timer",
+    keywords="pomodoro tomato tomato-timer terminal terminal-app pomodoro-timer tomato-ti",
     url="https://github.com/coolcode/tomato-clock",
     classifiers=['Intended Audience :: Science/Research',
                  'Intended Audience :: Developers',
@@ -37,5 +39,6 @@ setup(
     packages=find_packages(),
     entry_points={"console_scripts": ["tomato = tomato:main"]},
     setup_requires=["wheel"],
-    scripts=['tomato.py']
+    scripts=['tomato.py'],
+    install_requires=["plyer"] if sys.platform.startswith("win") else []
 )
